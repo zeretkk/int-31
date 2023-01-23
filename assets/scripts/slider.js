5a1b7bec909dbe703b4db62ddc90378dfa1887f6
zeret <kirill03192@gmail.com>
2023-01-23 18:00:04 +0300

    Separate js modules

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,41 +1,30 @@
-window.onload = function(){
-
-    const initSlideHandler =()=>{
-        let lastCall = 0
-        return function handleSlide(side, e){
-            if(lastCall > 0 && e.timeStamp - lastCall < 500) return
-            const container = document.querySelector('.slider__content')
-            let current = + container.dataset.current
-            const displayed = Math.floor(container.offsetWidth/(container.children[0].offsetWidth))
-            isSliding = true
-            switch(side){
-                case 'right':
-                    if(current >= container.children.length-displayed){
-                        container.scrollLeft = 0
-                        container.dataset.current = 0
-                        break
-                    }
-                    container.scrollBy(container.children[0].offsetWidth+container.offsetWidth/100*4, 0)
-                    container.dataset.current = current+1
+export const initSlideHandler =()=>{
+    let lastCall = 0
+    return function handleSlide(side, e){
+        if(lastCall > 0 && e.timeStamp - lastCall < 500) return
+        const container = document.querySelector('.slider__content')
+        let current = + container.dataset.current
+        const displayed = Math.floor(container.offsetWidth/(container.children[0].offsetWidth))
+        switch(side){
+            case 'right':
+                if(current >= container.children.length-displayed){
+                    container.scrollLeft = 0
+                    container.dataset.current = 0
                     break
-                case 'left':
-                    if(current <= 0){
-                        container.scrollLeft = container.scrollWidth
-                        container.dataset.current = container.children.length -1
-                        break
-                    }
-                    container.scrollBy(-container.children[0].offsetWidth-container.offsetWidth/100*4, 0)
-                    container.dataset.current = current - 1
+                }
+                container.scrollBy(container.children[0].offsetWidth+container.offsetWidth/100*4, 0)
+                container.dataset.current = current+1
+                break
+            case 'left':
+                if(current <= 0){
+                    container.scrollLeft = container.scrollWidth
+                    container.dataset.current = container.children.length -1
                     break
-            }
-            lastCall = e.timeStamp
+                }
+                container.scrollBy(-container.children[0].offsetWidth-container.offsetWidth/100*4, 0)
+                container.dataset.current = current - 1
+                break
         }
+        lastCall = e.timeStamp
     }
-
-    const handleSlide = initSlideHandler()
-    const sliderButtons = document.querySelectorAll('.slider__button')
-    sliderButtons.forEach(element=>{
-        element.addEventListener('click', (event)=>handleSlide(element.dataset.direction, event))
-    })
-
 }
